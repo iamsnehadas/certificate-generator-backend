2c0529cdfad109a9a0624508a131a2c1548a6ec6
iamsnehadas <snehasnow8@gmail.com>
2024-11-30 23:31:16 +0530

    update

--- server.js
+++ server.js
@@ -7,14 +7,23 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://certificate-generator-frontend-hazel.vercel.app", // Allow frontend URL
+    methods: ["GET", "POST"], // Allowed methods
+    credentials: true, // Support credentials if needed
+  })
+);
 
 // MongoDB Connection
 mongoose
-  .connect("mongodb+srv://snehadas0819:dTNxextxRMBsNZOM@cluster0.fbrtq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://snehadas0819:dTNxextxRMBsNZOM@cluster0.fbrtq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
@@ -22,5 +31,7 @@ mongoose
 app.use("/api/certificates", certificateRoutes);
 
 // Server
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+const PORT = 5000; // Local development port
+app.listen(PORT, () =>
+  console.log(`Server running at https://certificate-generator-backend-orcin.vercel.app/`)
+);
